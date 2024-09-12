913a49ad75b6f256b44ddf5d4dbd8ceca458633e
Roman Druzhinin <karamount11@gmail.com>
2024-09-13 02:55:24 +0300

    feature: типизированные хуки redux
    * Созданы два типизированных хука из redux
    * Добавлена типизация стора

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import { rootReducer } from "./reducers/rootReducer";
 
 export const createStore = () => {
@@ -6,3 +6,14 @@ export const createStore = () => {
     reducer: rootReducer,
   });
 };
+
+const store = createStore();
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
