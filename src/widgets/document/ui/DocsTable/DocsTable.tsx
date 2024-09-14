0d525fef0eac4e8c81d40afa7e5ac8ecd9afe877
Roman Druzhinin <karamount11@gmail.com>
2024-09-14 21:17:07 +0300

    feature: добавление иконок и кнопок в таблицу
    *Установлена бибдиотека с иконками MUI

--- src/widgets/document/ui/DocsTable/DocsTable.tsx
+++ src/widgets/document/ui/DocsTable/DocsTable.tsx
@@ -1,7 +1,9 @@
 import { FC, useEffect } from "react";
 import {
   Box,
+  Button,
   CircularProgress,
+  IconButton,
   Paper,
   Table,
   TableBody,
@@ -12,6 +14,7 @@ import {
 import { useAppDispatch, useAppSelector } from "@/shared/utils";
 import { getDocs } from "@/features";
 import { StyledTableCell, StyledTableRow } from "@/shared/ui";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const DocsTable: FC = () => {
   const { docs, isLoading, error } = useAppSelector((store) => store.docs);
@@ -35,22 +38,29 @@ const DocsTable: FC = () => {
 
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table" stickyHeader>
+      <Table sx={{ minWidth: 1200 }} aria-label="customized table" stickyHeader>
         <TableHead>
           <TableRow>
+            <StyledTableCell></StyledTableCell>
             <StyledTableCell>Имя документа</StyledTableCell>
             <StyledTableCell align="right">Тип документа</StyledTableCell>
             <StyledTableCell align="right">Статус документа</StyledTableCell>
-            <StyledTableCell align="right">Компания</StyledTableCell>
+            <StyledTableCell align="right">Подпись компании</StyledTableCell>
             <StyledTableCell align="right">Дата подписи</StyledTableCell>
             <StyledTableCell align="right">Номер сотрудника</StyledTableCell>
             <StyledTableCell align="right">Подпись сотрудника</StyledTableCell>
             <StyledTableCell align="right">Дата подписи</StyledTableCell>
+            <StyledTableCell align="right"></StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {docs.map((document) => (
             <StyledTableRow key={document.id}>
+              <StyledTableCell>
+                <Button variant="contained" size="small">
+                  Изменить
+                </Button>
+              </StyledTableCell>
               <StyledTableCell component="th" scope="row">
                 {document.documentName}
               </StyledTableCell>
@@ -74,6 +84,11 @@ const DocsTable: FC = () => {
               </StyledTableCell>
               <StyledTableCell align="right">
                 {document.employeeSigDate.slice(0, 10)}
+              </StyledTableCell>
+              <StyledTableCell>
+                <IconButton aria-label="delete" color="error" size="small">
+                  <DeleteIcon />
+                </IconButton>
               </StyledTableCell>
             </StyledTableRow>
           ))}
