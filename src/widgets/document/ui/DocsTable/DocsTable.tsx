ec145365020937f23e0124ddeba2a46a1ccbfe8d
Roman Druzhinin <karamount11@gmail.com>
2024-09-15 18:42:37 +0300

    feature: пагинация таблицы

--- src/widgets/document/ui/DocsTable/DocsTable.tsx
+++ src/widgets/document/ui/DocsTable/DocsTable.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import {
   Button,
   CircularProgress,
@@ -7,16 +7,19 @@ import {
   Table,
   TableBody,
   TableContainer,
+  TableFooter,
   TableHead,
+  TablePagination,
   TableRow,
 } from "@mui/material";
-import { useAppDispatch, useAppSelector } from "@/shared/utils";
+import { cutText, useAppDispatch, useAppSelector } from "@/shared/utils";
 import { getDocs } from "@/features";
 import { CustomModal, StyledTableCell, StyledTableRow } from "@/shared/ui";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import { openModal } from "../../model/slice/docsModalSlice";
 import styles from "./DocsTable.module.scss";
+import TablePaginationActions from "@mui/material/TablePagination/TablePaginationActions";
 
 const DocsTable: FC = () => {
   const { docs, isLoading, error } = useAppSelector((store) => store.docs);
@@ -26,6 +29,27 @@ const DocsTable: FC = () => {
     dispatch(getDocs());
   }, []);
 
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - docs.length) : 0;
+
+  const handleChangePage = (
+    //Не удалять событие
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   if (error) {
     <h1>Ошибка загрузки документов, перезагрузите страницу</h1>;
   }
@@ -33,7 +57,7 @@ const DocsTable: FC = () => {
   if (isLoading) {
     return (
       <CustomModal open>
-        <CircularProgress className={styles.table__loader}/>
+        <CircularProgress className={styles.table__loader} />
       </CustomModal>
     );
   }
@@ -65,7 +89,10 @@ const DocsTable: FC = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {docs.map((document) => (
+          {(rowsPerPage > 0
+            ? docs.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : docs
+          ).map((document) => (
             <StyledTableRow key={document.id}>
               <StyledTableCell align="center">
                 <IconButton
@@ -85,25 +112,25 @@ const DocsTable: FC = () => {
                 </IconButton>
               </StyledTableCell>
               <StyledTableCell component="th" scope="row">
-                {document.documentName}
+                {cutText(7, document.documentName)}
               </StyledTableCell>
               <StyledTableCell align="right">
-                {document.documentType}
+                {cutText(7, document.documentType)}
               </StyledTableCell>
               <StyledTableCell align="right">
-                {document.documentStatus}
+                {cutText(7, document.documentStatus)}
               </StyledTableCell>
               <StyledTableCell align="right">
-                {document.companySignatureName}
+                {cutText(7, document.companySignatureName)}
               </StyledTableCell>
               <StyledTableCell align="right">
                 {document.companySigDate.slice(0, 10)}
               </StyledTableCell>
               <StyledTableCell align="right">
-                {document.employeeNumber}
+                {cutText(7, document.employeeNumber)}
               </StyledTableCell>
               <StyledTableCell align="right">
-                {document.employeeSignatureName}
+                {cutText(7, document.employeeSignatureName)}
               </StyledTableCell>
               <StyledTableCell align="right">
                 {document.employeeSigDate.slice(0, 10)}
@@ -127,7 +154,34 @@ const DocsTable: FC = () => {
               </StyledTableCell>
             </StyledTableRow>
           ))}
+          {emptyRows > 0 && (
+            <StyledTableRow style={{ height: 53 * emptyRows }}>
+              <StyledTableCell colSpan={10} />
+            </StyledTableRow>
+          )}
         </TableBody>
+        <TableFooter className={styles.table__footer}>
+          <StyledTableRow>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 50, { label: "All", value: -1 }]}
+              colSpan={10}
+              count={docs.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              slotProps={{
+                select: {
+                  inputProps: {
+                    "aria-label": "rows per page",
+                  },
+                  native: true,
+                },
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </StyledTableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   );
